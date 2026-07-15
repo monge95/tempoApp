9dde20034006007d554b4ea907b28f8fe17a3e14
monge <pedrokamonge@gmail.com>
2026-07-15 16:02:49 -0300

    Initial Commit

---
+++ tempo/tempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempoApp.swift
+//  tempo
+//
+//  Created by Pedro Monge Silveira on 15/07/26.
+//
+
+import SwiftUI
+
+@main
+struct tempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
