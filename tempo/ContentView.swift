4cc11f3c4edfb00edc9fc53b9be9455277373f6a
monge <pedrokamonge@gmail.com>
2026-07-19 10:54:50 -0300

    creat repo clima App

--- tempo/ContentView.swift
+++ tempo/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image("fundo")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            ScrollView{
+                
+            }
+           
         }
-        .padding()
     }
 }
 
